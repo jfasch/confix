3640af8e4935aac52f479d528d01dea1a521f7ac
Joerg Faschingbauer <jf@faschingbauer.co.at>
2006-07-29 21:27:42 +0000

    *** empty log message ***

--- tests/c/inter_package.py
+++ tests/c/inter_package.py
@@ -17,10 +17,11 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 # USA
 
-from libconfix.testutils import dirhier
+from libconfix.testutils import dirhier, find
 from libconfix.core.filesys.file import File
 from libconfix.core.local_package import LocalPackage
 from libconfix.plugins.c.setup import CSetupFactory
+from libconfix.plugins.c.library import LibraryBuilder
 
 import unittest
 
@@ -36,20 +37,22 @@ class InterPackageRelate(unittest.TestCase):
         lofs = dirhier.packageroot(name='lo', version='1.2.3')
         lofs.rootdirectory().add(name='lo.h', entry=File())
         lofs.rootdirectory().add(name='lo.c', entry=File())
-        local_lopkg = LocalPackage(root=lofs.rootdirectory(), setups=[CSetupFactory(short_libnames=False,
-                                                                                    use_libtool=False)])
+        local_lopkg = LocalPackage(root=lofs.rootdirectory(),
+                                   setups=[CSetupFactory(short_libnames=False, use_libtool=False)])
         local_lopkg.enlarge(external_nodes=[])
         installed_lopkg = local_lopkg.install()
 
         hifs = dirhier.packageroot(name='hi', version='1.2.3')
-        hifs.rootdirectory().add(name='hi.c', entry=File())
-        local_hipkg = LocalPackage(root=hifs.rootdirectory(), setups=[CSetupFactory()])
+        hifs.rootdirectory().add(name='hi.c',
+                                 entry=File(lines=['#include <lo.h']))
+        local_hipkg = LocalPackage(root=hifs.rootdirectory(),
+                                   setups=[CSetupFactory(short_libnames=False, use_libtool=False)])
         local_hipkg.enlarge(external_nodes=installed_lopkg.nodes())
 
-        lo_h_builder = find.find_entrybuilder(root=local_hipkg.rootbuilder(), path=['lo.h'])
-        lo_c_builder = find.find_entrybuilder(root=local_hipkg.rootbuilder(), path=['lo.c'])
+        lo_h_builder = find.find_entrybuilder(root=local_lopkg.rootbuilder(), path=['lo.h'])
+        lo_c_builder = find.find_entrybuilder(root=local_lopkg.rootbuilder(), path=['lo.c'])
         liblo_builder = None
-        for b in local_hipkg.rootbuilder().builders():
+        for b in local_lopkg.rootbuilder().builders():
             if isinstance(b, LibraryBuilder):
                 liblo_builder = b
                 break
@@ -58,7 +61,22 @@ class InterPackageRelate(unittest.TestCase):
             self.fail()
             pass
 
-        self.failUnless(len(lo_c_builder.native_installed_modules_used()) == 1)
+        hi_c_builder = find.find_entrybuilder(root=local_hipkg.rootbuilder(), path=['hi.c'])
+        libhi_builder = None
+        for b in local_hipkg.rootbuilder().builders():
+            if isinstance(b, LibraryBuilder):
+                libhi_builder = b
+                break
+            pass
+        else:
+            self.fail()
+            pass
+
+        # hi.c includes lo.h, so it must have a BuildInfo for
+        # installed header files, but none for local header files.
+        self.failUnless(len(hi_c_builder.native_installed_modules_used()) == 1)
+        self.failIf(len(hi_c_builder.native_local_modules_used()) != 0)
+
         for bi in liblo_builder.buildinfo_direct_dependent_libs():
             if isinstance(bi, BuildInfo_CLibrary_NativeInstalled) and bi.name() == 'lo':
                 break
