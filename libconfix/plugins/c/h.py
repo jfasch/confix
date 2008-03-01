d6ccd99730e35533ff0c759213809c502cbbc9f6
Joerg Faschingbauer <jf@faschingbauer.co.at>
2008-03-01 21:28:10 +0000

    *** empty log message ***

--- libconfix/plugins/c/h.py
+++ libconfix/plugins/c/h.py
@@ -16,7 +16,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 # USA
 
-jjj factor this out into sutomake
+jjj factor this out into automake
 
 from base import CBaseBuilder
 from dependency import Provide_CInclude
